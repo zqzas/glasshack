52149c03b96a1b748e2172d7960abd2dcf4ef7e1
cxhiano <cxhiano@gmail.comm>
2014-03-02 02:24:34 +0800

    raw api

--- tunnel.py
+++ tunnel.py
@@ -1,11 +1,11 @@
 import thread
-from flask import request
-import app
+from flask import request, session
+from app import app
 import urllib2
 
 tl_ids = []
 
-@app.app.subscriptions.action('SHARE')
+@app.web.route('/callback')
 def share(user):
     lst = user.timeline.list()
     print lst
@@ -13,11 +13,21 @@ def share(user):
         if not tl['id'] in tl_ids:
             tl_ids.append(tl['id'])
             print tl
-            #thread.start_new_thread(process, (tl['contentUrl'])
+            url = 'http://hooin.qiniudn.com/IMG_1497.JPG'
+            #thread.start_new_thread(render_id_cards, url)# (tl['contentUrl'])
 
-def process(url):
+def render_id_cards(url):
     api = 'http://192.168.2.169:3000/misc/facematch'
     result = urllib2.urlopen(api + '?photo=' + url).read()
+    session['user'].timeline.post(html=render_template(result))
+
+def subscrip(user):
+    user.request("POST", "/mirror/v1/subscriptions", data=json.dumps({
+        "collection": "timeline",
+        "userToken": user.token,
+        "operation": "INSERT",
+        "callbackUrl": "%s/callback" % self.app.host,
+      }))
 
 if __name__ == '__main__':
     url = 'http://hooin.qiniudn.com/IMG_1497.JPG'
