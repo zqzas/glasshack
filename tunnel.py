964fd95d75f96b4539e37589496bf60f655b144b
cxhiano <cxhiano@gmail.comm>
2014-03-02 04:04:57 +0800

    get_img_url

--- tunnel.py
+++ tunnel.py
@@ -3,6 +3,23 @@ from flask import request, session
 from app import app
 import urllib2
 
+def get_img_url(user, itemid, attachment_id):
+    r = user.request("GET", "https://www.googleapis.com/mirror/v1/timeline/%s/attachments/%s" % (itemid, attachment_id))
+    print r
+    return json.loads(r)['contentUrl']
+
+@app.web.route('/share_callback')
+def share():
+    print 'fuckyou'
+    lst = userObj.timeline.list()
+    print lst
+    for tl in lst:
+        if not tl['id'] in tl_ids:
+            global tl_ids
+            tl_ids.append(tl['id'])
+            url = get_img_url(userObj, tl['id'], tl['attachments']['id'])
+            thread.start_new_thread(render_id_cards, url)
+
 def render_id_cards(url):
     api = 'http://192.168.2.169:3000/misc/facematch'
     result = urllib2.urlopen(api + '?photo=' + url).read()
