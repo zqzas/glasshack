59baa950a7497e65487ecf3f3b818732e274f0b6
gugugupan <gugugupan@163.com>
2014-03-02 03:49:46 +0800

    fix for google login

--- glasshach_api/app/controllers/accounts_controller.rb
+++ glasshach_api/app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
 
 	def index
 		oauth_params = {
-				:scope => "profile" ,
+				:scope => "https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email" ,
 				:state => "" ,
 				:redirect_uri => "http://#{ request.host_with_port }/accounts/google_callback" ,
 				:response_type => "code" ,
@@ -10,7 +10,6 @@ class AccountsController < ApplicationController
 				:approval_prompt => "force"
 			}
 		@google_oauth_path = "https://accounts.google.com/o/oauth2/auth?#{ oauth_params.to_param }"
-		@google_oauth_path = "#"
 	end
 
 	def register
@@ -21,9 +20,7 @@ class AccountsController < ApplicationController
 	end
 
 	def google_callback
-		params[ :code ]
-		"https://oauth2-login-demo.appspot.com/code?state=/profile&code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7"
-		connection = Faraday.new( :url => "https://oauth2-login-demo.appspot.com/" )
+		connection = Faraday.new( :url => "https://accounts.google.com/" )
 		oauth_params = {
 				:code => params[ :code ] ,
 				:client_id => Google.GOOGLE_CLIENT_ID ,
@@ -31,8 +28,24 @@ class AccountsController < ApplicationController
 				:redirect_uri => "http://#{ request.host_with_port }/accounts/google_callback" ,
 				:grant_type => "authorization_code"
 			}
-		response = connection.post( "/code" , oauth_params )
+		response = connection.post( "/o/oauth2/token" , oauth_params )
+		puts "[GOOGLE OAUTH LOGIN]"
 		puts response.body
 		token = JSON.parse( response.body )
+
+		connection2 = Faraday.new( :url => "https://www.googleapis.com/" )
+		response2 = connection2.get( "/plus/v1/people/me" , { :access_token => token[ "access_token" ] } )
+
+		pinfo = JSON.parse( response2.body )
+
+		redirect_params = {
+				:fname => pinfo[ "name" ] [ "givenName" ] ,
+				:lname => pinfo[ "name" ] [ "familyName" ] ,
+				:gender => pinfo[ "gender" ] ,
+				:email => pinfo[ "emails" ] [ 0 ] [ "value" ] ,
+				:age => pinfo[ "ageRange" ] [ "min" ] 
+			}
+
+		redirect_to( "/accounts/register?#{ redirect_params.to_param }" )
 	end
 end
