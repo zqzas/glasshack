1918ddd875252e0cf13a8a97fa2cf64ca036172d
cxhiano <cxhiano@gmail.comm>
2014-03-01 21:53:47 +0800

    get photo v2

--- tunnel.py
+++ tunnel.py
@@ -4,23 +4,18 @@ import app
 
 tl_ids = []
 
-def get_image(tl):
-     #r = self.user.request("GET", "/mirror/v1/timeline/", data = {})
-     print tl
-
 @app.app.subscriptions.action('SHARE')
 def share(user):
-    for tl in user.timeline.list():
+    lst = user.timeline.list()
+    print lst
+    for tl in lst:
         if not tl['id'] in tl_ids:
-            tl_ids.append(tl)
-            img = get_image(tl['id'])
-            #thread.start_new_thread(process, (img, ))
+            tl_ids.append(tl['id'])
+            print tl
+            #thread.start_new_thread(process, (tl['contentUrl'])
 
-def display_card():
+def process(url):
     pass
-
-def process(img):
-    display_card()
 
 if __name__ == '__main__':
     pass
